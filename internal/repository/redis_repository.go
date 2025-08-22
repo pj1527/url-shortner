050c0d7173d3887a621f8583a6c8601235989375
PJ1527 <priyamjain1527@gmail.com>
2025-08-22 18:55:12 +0530

    using lua scripts for robustness and reduce network latency

--- internal/repository/redis_repository.go
+++ internal/repository/redis_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"fmt"
+	"log"
 	"strconv"
 	"time"
 
@@ -10,8 +11,15 @@ import (
 )
 
 type redisRepository struct {
-	client *redis.Client
+	client        *redis.Client
+	saveURLScript *redis.Script
 }
+
+const saveURLScriptBody = `
+    local id = redis.call('INCR', KEYS[1])
+    redis.call('SET', 'url:' .. id, ARGV[1])
+    return id
+`
 
 func NewRedisRepository(addr string) (Repository, error) {
 	client := redis.NewClient(&redis.Options{
@@ -27,21 +35,26 @@ func NewRedisRepository(addr string) (Repository, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to Redis: %v", err)
 	}
+	script := redis.NewScript(saveURLScriptBody)
 	return &redisRepository{
-		client: client,
+		client:        client,
+		saveURLScript: script,
 	}, nil
 }
 
 func (r *redisRepository) SaveURL(ctx context.Context, longURL string) (uint64, error) {
-	id, err := r.client.Incr(ctx, "url_counter").Uint64()
+	keys := []string{"url_counter"}
+	args := []any{longURL}
+	result, err := r.saveURLScript.Run(ctx, r.client, keys, args...).Result()
 	if err != nil {
-		return 0, err
+		log.Println("Failed to execute redis script", err)
+		return 0, fmt.Errorf("failed to execute redis script")
 	}
-	err = r.client.Set(ctx, r.getKey(id), longURL, 0).Err()
-	if err != nil {
-		return 0, err
+	id, ok := result.(int64)
+	if !ok {
+		return 0, fmt.Errorf("unexpected result type from redis script: %T", result)
 	}
-	return id, nil
+	return uint64(id), nil
 }
 
 func (r *redisRepository) GetURL(ctx context.Context, id uint64) (string, bool) {
